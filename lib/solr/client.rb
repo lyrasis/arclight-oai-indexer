8fa1a21f8843d72b271254db47451f2cdea3e9b5
Mark Cooper <markchristophercooper@gmail.com>
2019-05-30 13:02:34 -0700

    Support retry for solr connect errs

--- lib/solr/client.rb
+++ lib/solr/client.rb
@@ -2,11 +2,12 @@
 
 module Solr
   class Client
-    attr_reader :endpoint, :indexer, :solr
+    attr_reader :endpoint, :indexer, :logger, :solr
 
-    def initialize(endpoint: nil, indexer: nil)
+    def initialize(endpoint: nil, indexer: nil, logger: nil)
       @endpoint = endpoint
       @indexer  = indexer
+      @logger   = logger
       @solr     = connect
     end
 
@@ -15,13 +16,15 @@ module Solr
     end
 
     def delete(eadid: nil)
-      solr.get(
-        'select',
-        params: {
-          q: solr.delete_by_query("ead_ssi:#{escape(eadid)}")
-        }
-      )
-      solr.commit
+      with_retry do
+        solr.get(
+          'select',
+          params: {
+            q: solr.delete_by_query("ead_ssi:#{escape(eadid)}")
+          }
+        )
+        solr.commit
+      end
     end
 
     def escape(string)
@@ -29,7 +32,24 @@ module Solr
     end
 
     def index(file: nil)
-      indexer.update(file)
+      with_retry do
+        indexer.update(file)
+      end
+    end
+
+    def with_retry
+      attempts = 0
+      loop do
+        begin
+          yield
+          break
+        rescue RSolr::Error::ConnectionRefused => ex
+          raise ex if attempts == 5
+
+          logger.warn("Retrying connection to Solr: #{ex.message}")
+          sleep((attempts += 1) * 30)
+        end
+      end
     end
   end
 end
