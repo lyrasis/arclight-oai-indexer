429124f34d8c7051625653f7c821e06916fbb8cf
Mark Cooper <markchristophercooper@gmail.com>
2019-05-30 15:39:13 -0700

    Monkey patch pagination for retry

--- requirements.rb
+++ requirements.rb
@@ -17,3 +17,37 @@ require_relative 'lib/arclight/indexer'
 require_relative 'lib/solr/client'
 require_relative 'lib/utils/file'
 require_relative 'lib/utils/oai'
+
+module Fieldhand
+  class Paginator
+    # https://github.com/fieldhand/fieldhand/issues/17
+    def items(verb, parser_class, query = {}) # rubocop:disable all
+      return enum_for(:items, verb, parser_class, query) unless block_given?
+
+      redos = 0
+      loop do
+        begin
+          response_parser = parse_response(query.merge('verb' => verb))
+        rescue ResponseError => e
+          redos += 1
+          if redos < 10 # rubocop:disable Style/GuardClause
+            delay = redos * redos
+            logger.warn('Fieldhand') { "Response error #{e}\nRetrying in #{delay} seconds" }
+            sleep delay
+            redo
+          else
+            raise e
+          end
+        end
+        parser_class.new(response_parser).items.each do |item|
+          yield item
+        end
+
+        break unless response_parser.resumption_token
+
+        logger.debug('Fieldhand') { "Resumption token for #{verb}: #{response_parser.resumption_token}" }
+        query = { 'resumptionToken' => response_parser.resumption_token }
+      end
+    end
+  end
+end
