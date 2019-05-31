a7ba250252e07dc52d2c92b4a0f2598c1122fc48
Mark Cooper <markchristophercooper@gmail.com>
2019-05-31 12:41:59 -0700

    Make parser more permissive for wild EAD

--- requirements.rb
+++ requirements.rb
@@ -54,4 +54,16 @@ module Fieldhand
       end
     end
   end
+
+  class ResponseParser
+    # https://github.com/fieldhand/fieldhand/blob/master/lib/fieldhand/response_parser.rb#L56
+    def root
+      @root ||= ::Ox.load(
+        response,
+        effort: :tolerant,
+        smart: true,
+        strip_namespace: 'oai'
+      ).root
+    end
+  end
 end
